f128738adf8cda87a998eb243a5d1f63bb3d7cea
edofiorini <66549725+edofiorini@users.noreply.github.com>
2022-03-19 14:24:56 +0100

    add test folder

--- cav_estimator.m
+++ cav_estimator.m
@@ -9,19 +9,19 @@ method = MethodName.Fiore;
 modelFile = 'models/refDescriptorsCav';
 load(modelFile); %referenceModel variable
 
-for i = 1:4:13
-checkImageFile = "cav/cav_test_"+num2str(i)+".jpg";
+for i = 1:6
+checkImageFile = "cav/test//cav_test_"+num2str(i)+".jpg";
 testK = getInternals(checkImageFile); % estimated internal params of test image
 [R, T] = pose_estimator(referenceModel, checkImageFile, method, testK);
 if i == 1
     figure(100)
     scatter3(referenceModel.p3D(:,1),referenceModel.p3D(:,2),referenceModel.p3D(:,3),5,'r');
     hold on
-    plotCameraOnImage(referenceModel.R, referenceModel.T, 'ref');
+    plotCameraOnImage(referenceModel.R, referenceModel.T, '  ref');
 end
 figure(100)
 hold on;
-plotCameraOnImage(R, T, num2str(i));
+plotCameraOnImage(R, T, "  " + num2str(i));
 axis equal
 end
 
--- dante_estimator.m
+++ dante_estimator.m
@@ -8,20 +8,20 @@ run('functions/sift/toolbox/vl_setup');
 method = MethodName.Fiore;
 modelFile = 'models/refDescriptorsDante1020';
 load(modelFile); %variable referenceModel
-checkImageFile = 'dante/test_4.jpg';
+checkImageFile = 'dante/test/test_4.jpg';
 % checkImageFile = 'Zephyr_Dante_Statue_Dataset/_SAM1097.JPG';
 testK = getInternals(checkImageFile); % estimated internal params of test image
 
 [R1, T1] = pose_estimator(referenceModel, checkImageFile, method, testK);
 
-ptCloud = pcread('Mesh.ply');
+ptCloud = pcread('dante/Mesh.ply');
 figure()
 pcshow(ptCloud)
 set(gcf,'color','w');
 set(gca,'color','w');
 set(gca, 'XColor', [0.15 0.15 0.15], 'YColor', [0.15 0.15 0.15], 'ZColor', [0.15 0.15 0.15]);
 hold on
-plotCameraOnImage(referenceModel.R, referenceModel.T, 'ref');
-plotCameraOnImage(R1, T1, '1');
+plotCameraOnImage(referenceModel.R, referenceModel.T, '  ref');
+plotCameraOnImage(R1, T1, '  1');
 
 axis equal
